beb4a08d6fe66db07da77abf782f2b3f70dcfc5d
Alexander7145 <aberofeevy@gmail.com>
2024-03-01 10:40:01 +0300

    СТРОКИ. Домашка.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         //task1
+        String firstMame = " Ivan ";
+        String middleName = " Ivanovich ";
+        String lastName = " Ivanov ";
+        String fullName = firstMame + "" + middleName + "" + lastName;
+        System.out.println(" Ф.И.О. сотрудника - " + fullName);
+        //task2
+        String fullNametoUpperCase = fullName.toUpperCase();
+        System.out.println(" Данные Ф.И.О. сотрудника для заполнения отчете " + fullNametoUpperCase);
+        //task3
+        String newFullName = " Иванов Семён Семёнович ";
+        System.out.println(" Данные Ф.И.О. сотрудника " + newFullName.replace('ё', 'е'
+        ));
+
 
     }
 }
